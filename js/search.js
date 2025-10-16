7ed142170d0b557e32c62b96e29d666870f89faa
mirasto <maksimorozov@protonmail.com>
2025-10-17 00:47:22 +0300

    add modal window

--- js/search.js
+++ js/search.js
@@ -1,11 +1,33 @@
 import { BASE_URL } from './api';
 import axios from 'axios';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// 1. Отримуємо посилання на DOM-елементи
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 
+// 2. Ініціалізуємо SimpleLightbox для галереї
+let modal = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 0,
+  overlay: true,
+  spinner: true,
+  nav: true,
+  close: true,
+});
+modal.on('show.simplelightbox', function (e) {
+  const imageURL = e.target.dataset.modal;
+  console.log(imageURL);
+});
+
+// 3. Додаємо обробник події на форму пошуку
 form.addEventListener('submit', searchImage);
 
+// 4. Функція для побудови URL для запиту
 function buildSearchUrl(query) {
   const params = [
     `q=${encodeURIComponent(query)}`,
@@ -15,6 +37,8 @@ function buildSearchUrl(query) {
   ].join('&');
   return `${BASE_URL}&${params}`;
 }
+
+// 5. Обробник події submit форми
 function searchImage(e) {
   e.preventDefault();
 
@@ -24,6 +48,7 @@ function searchImage(e) {
   getImages(buildSearchUrl(searchQueryValue));
 }
 
+// 6. Асинхронна функція для отримання зображень з API
 async function getImages(url) {
   try {
     const response = await axios.get(url);
@@ -38,22 +63,25 @@ async function getImages(url) {
   }
 }
 
+// 7. Функція для рендеру галереї
 function renderGallery(data) {
+  if (!data.length) {
+    // need create notify for error
+  }
   const markup = galleryMarkup(data);
   gallery.insertAdjacentHTML('afterbegin', markup);
+  modal.refresh();
 }
-gallery.addEventListener('click', e => {
-  if (e.target.tagName == 'IMG') {
-    const imageURL = e.target.dataset.modal;
-  }
-});
 
+// 8. Функція для створення розмітки галереї
 function galleryMarkup(data) {
   const images = data.hits;
   return images
     .map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) => {
       return `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" data-modal="${largeImageURL}">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy">
+        </a>
         <div class="info">
           <p class="info-item"><b>Likes:</b> ${likes}</p>
           <p class="info-item"><b>Views:</b> ${views}</p>
